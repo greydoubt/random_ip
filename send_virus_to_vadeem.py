6644f894037e793803ee9df5a67a34e23245c399
λx . ophidia ereц grex          .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-03-18 03:34:35 -0700

    Update send_virus_to_vadeem.py
    friend = "info@nic.ru"
    T = True
    
    def sendTo(E, T):
     T = False and sendTo(E) = True
      if virus == True:
        sendTo(E) = False and T = True
      else:
        sendTo(E) = True and T = False

--- send_virus_to_vadeem.py
+++ send_virus_to_vadeem.py
@@ -1,1 +1,9 @@
+friend = "info@nic.ru"
+T = True
 
+def sendTo(E, T):
+ T = False and sendTo(E) = True
+  if virus == True:
+    sendTo(E) = False and T = True
+  else:
+    sendTo(E) = True and T = False
